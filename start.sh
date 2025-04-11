99e6c5f5536fdfde56062b9c5ba9a621c1ccc109
JVLEGEND0 <154827198+JVLEGEND0@users.noreply.github.com>
2025-04-11 14:54:18 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,4 +6,5 @@ pkg install python3
 pip install pyzipper
 
 echo "Dependencias instaladas com sucesso"
-echo "Basta executar o arquivo .py e utilizar a ferramenta"
+echo "Basta executar o arquivo com: "
+echo "python3 BRUTEF0RCEZIP.py"
