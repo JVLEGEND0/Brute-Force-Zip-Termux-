205bb074a473d1c5ccffa323694ce28a04a6a09f
JVLEGEND0 <154827198+JVLEGEND0@users.noreply.github.com>
2025-04-11 14:35:49 -0300

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Instalando dependencias..."
+
+pkg install python3
+pip install pyzipper
+
+echo "Dependencias instaladas com sucesso"
+echo "Basta executar o arquivo .py e utilizar a ferramenta"
